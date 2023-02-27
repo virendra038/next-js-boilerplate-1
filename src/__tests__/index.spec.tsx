02f39bb8b00fc4a25c6cfcbb20a6492bccca7264
virendra038 <virendrapratap038@gmail.com>
2023-02-27 17:12:18 +0530

    first commit

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/pages/index'
+import '@/styles/global.css'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: "Hello World!",
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
