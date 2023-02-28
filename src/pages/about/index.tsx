03b98b1fc046d5a215833db54af67e84cfa65c47
virendra038 <virendrapratap038@gmail.com>
2023-02-28 17:05:36 +0530

    chakra setup

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import { Link } from "@chakra-ui/react";
 
 export default function About() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <Link href="/">Homepage</Link>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Link href="/">Homepage</Link>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
+import { Link } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
