6dd3e47c2b5fc0b4d36ef25371efaf57fe4af62e
vishal <vishalgiri8507012571@gmail.com>
2023-03-02 18:03:19 +0530

    ESY-1588

---
+++ src/database/todoCol.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let todoSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true,
+    },
+    priority: {
+        type: String,
+        enum : ['High','Medium',"Low"],
+        required: true,
+    },
+    dueDate: {
+        type: Date,
+        required: true, 
+    },
+    done: {
+        type: Boolean,
+        required:true
+    },
+},
+{
+    timestamps:true
+});
+
+let todoCol = mongoose.models.todoCollection || mongoose.model("todoCollection", todoSchema);
+
+export default todoCol;
