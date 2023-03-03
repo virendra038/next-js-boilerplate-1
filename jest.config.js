5b68b73aa32ebd1dfc603236536a999f450a0e19
virendra038 <virendrapratap038@gmail.com>
2023-03-03 18:10:10 +0530

    adds .env.development, installs supertest, fixes issues in jest.config file

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
     "^@/components/(.*)$": "<rootDir>/src/components/$1",
 
     "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+    "@/database/(.*)$": "<rootDir>/src/database/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
