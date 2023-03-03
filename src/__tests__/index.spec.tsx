39a6e64a51669eaa903cc4ad6019cfe4d9e716d8
adiii013 <adidada023@gmail.com>
2023-03-03 17:53:56 +0530

    added test

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 import '@/styles/global.css'
+import Wrapper from '../utils/wrapper';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />)
+    render(<Home />,{wrapper:Wrapper})
   })
 })
