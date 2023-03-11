11f245782812391f813a5beb2e2b6fd7cee409e5
adiii013 <adidada023@gmail.com>
2023-03-12 00:16:22 +0530

    added changes

--- src/utils/wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-
-export default ThemeWrapper = ({ children }) => (
-  <ChakraProvider>{children}</ChakraProvider>
-);
