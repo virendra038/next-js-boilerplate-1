90eab21f59d3955184dbdd9a7ead5fede3cd51fa
adiii013 <adidada023@gmail.com>
2023-03-12 00:16:02 +0530

    removed test

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 import '@/styles/global.css'
-import Wrapper from '../utils/wrapper';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />,{wrapper:Wrapper})
+    render(<Home />)
   })
 })
