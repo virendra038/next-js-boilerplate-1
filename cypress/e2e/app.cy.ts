6db5608986ab75cb57ac8606d687d27f7dc88c14
adiii013 <adidada023@gmail.com>
2023-03-13 22:19:34 +0530

    Resolved confilts

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,11 +1,13 @@
 describe("Navigation", () => {
-  beforeEach(() => {
-    cy.visit("/");
+    beforeEach(() => {
+      cy.visit("/");
+    });
+  
+    it("should check for the text Hello World", () => {
+      cy.get("h2").should("contain", "Todo App");
+    });
+  
+    
   });
-
-  it("should check for the text Hello World", () => {
-    cy.get("h2").should("contain", "Todo App");
-  });
-});
-
-export {};
+  
+  export {};
