840566842a69ead64ebfe0afe3918e48b9dfb9da
vishal <vishalgiri8507012571@gmail.com>
2023-03-20 17:47:08 +0530

    api revamp 3

--- src/__tests__/pages/api/todo/[id].spec.ts
+++ src/__tests__/pages/api/todo/[id].spec.ts
@@ -21,7 +21,7 @@ describe("Testing with id route path",()=>{
                 "task": "API",
                 "priority": "High",
                 "dueDate": "2025-10-08",
-                "done": true
+                "done": false
             }
         });
         let response = httpMocks.createResponse();
@@ -51,8 +51,8 @@ describe("Testing with id route path",()=>{
             const response = httpMocks.createResponse();
             await app(request, response);
             const data = response._getData();
-            expect(response.statusCode).toEqual(406)
-            expect(data).toStrictEqual({message:"id length is not as per mongoose objectID"})    
+            expect(response.statusCode).toEqual(404)
+            expect(data).toStrictEqual({message:"Todo Data with given id cannot be found!"})    
         })
         test('GET/PUT/DELETE request with _id not present',async ()=>{
                 let id = String(new mongoose.Types.ObjectId());
@@ -61,7 +61,7 @@ describe("Testing with id route path",()=>{
                 await app(request, response);
                 const data = response._getData();
                 expect(response.statusCode).toEqual(404)
-                expect(data).toStrictEqual({message:"Data with given id not found!"})      
+                expect(data).toStrictEqual({message:"Todo Data with given id cannot be found!"})      
         })
     })
 
@@ -90,7 +90,7 @@ describe("Testing with id route path",()=>{
             });
             const response = httpMocks.createResponse();
             await app(request, response);
-            expect(response.statusCode).toEqual(204) 
+            expect(response.statusCode).toEqual(200) 
         })
     })
 
