c2b5ef53b5f10e6ef53c6327a435f7ebf9aefa65
Puneet Arora <puneet2715@gmail.com>
2023-03-21 20:29:08 +0530

    removed port and base url from env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL || "http://localhost",
-    PORT: process.env.PORT || 3000,
-  },
 };
 
 module.exports = nextConfig;
