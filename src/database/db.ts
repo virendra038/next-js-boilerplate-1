f133a39ac36591a98f1daf5ee256673fec586001
Puneet Arora <puneet2715@gmail.com>
2023-03-21 20:30:03 +0530

    reset db.ts

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,9 +5,6 @@ const connectDB = async () => {
   if (!mongoose.connections[0].readyState) {
     mongoose.set("strictQuery", true);
     await mongoose.connect(DB_URL);
-    console.log("Connected to MongoDB", DB_URL);
-    console.log("Base Url: ", process.env.BASE_URL);
-    console.log("Port: ", process.env.PORT);
   }
 };
 
