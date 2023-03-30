905ac8f476f2786303e313580feb7fe080c42b2f
virendra038 <virendrapratap038@gmail.com>
2023-03-30 19:50:44 +0530

    integration test poc-8

--- src/components/textController/inputForm.tsx
+++ src/components/textController/inputForm.tsx
@@ -96,7 +96,7 @@ function inputForm({onChangeInput,user,err,onSubmit,login}:any) {
                                 colorScheme="green"
                                 width="full"
                             >
-                               {login ? 'Login!!' : 'Continue'}
+                               {login ? 'Log' : 'Continue'}
                             </Button>
                             <Text>{err}</Text>
                         </Stack>
